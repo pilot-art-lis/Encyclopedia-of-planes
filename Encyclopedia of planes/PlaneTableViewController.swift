a9833953831b8c559a889a123c21fb5f4a684030
Artem Lysevych <alysevyc@mion.elka.pw.edu.pl>
2020-07-02 21:33:41 +0200

    Add new planes; compatibility for all iPhones

--- Encyclopedia of planes/PlaneTableViewController.swift
+++ Encyclopedia of planes/PlaneTableViewController.swift
@@ -19,6 +19,12 @@ class PlaneTableViewController: UITableViewController {
         let image1 = UIImage(named: "plane1")
         let image2 = UIImage(named: "plane2")
         let image3 = UIImage(named: "plane3")
+        let image4 = UIImage(named: "plane4")
+        let image5 = UIImage(named: "plane5")
+        let image6 = UIImage(named: "plane6")
+        let image7 = UIImage(named: "plane7")
+        let image8 = UIImage(named: "plane8")
+        let image9 = UIImage(named: "plane9")
         
         guard let plane1 = Plane(model: "Sukhoi Superjet 100", photo: image1, country: "Russia", weight: 49450, speed: 950, passengers: 108, distance: 4578) else { fatalError("Unable to instatiate plane1") }
         
@@ -26,7 +32,19 @@ class PlaneTableViewController: UITableViewController {
         
         guard let plane3 = Plane(model: "Boeing 737-900", photo: image3, country: "USA", weight: 79000, speed: 852, passengers: 189, distance: 5800) else { fatalError("Unable to instatiate plane3") }
         
-        planes += [plane1, plane2, plane3]
+        guard let plane4 = Plane(model: "Antonov An-225", photo: image4, country: "Ukraine", weight: 250000, speed: 850, passengers: 6, distance: 15400) else { fatalError("Unable to instatiate plane4") }
+        
+        guard let plane5 = Plane(model: "Antonov An-124", photo: image5, country: "Ukraine", weight: 178400, speed: 850, passengers: 8, distance: 7500) else { fatalError("Unable to instatiate plane5") }
+        
+        guard let plane6 = Plane(model: "Airbus A380", photo: image6, country: "France", weight: 590000, speed: 945, passengers: 525, distance: 15200) else { fatalError("Unable to instatiate plane6") }
+        
+        guard let plane7 = Plane(model: "Yak-40", photo: image7, country: "Russia", weight: 9850, speed: 546, passengers: 40, distance: 2500) else { fatalError("Unable to instatiate plane7") }
+        
+        guard let plane8 = Plane(model: "Boeing 787-9", photo: image8, country: "USA", weight: 126000, speed: 956, passengers: 290, distance: 14140) else { fatalError("Unable to instatiate plane8") }
+        
+        guard let plane9 = Plane(model: "Airbus A340-600", photo: image9, country: "France", weight: 380000, speed: 913, passengers: 475, distance: 14600) else { fatalError("Unable to instatiate plane9") }
+        
+        planes += [plane1, plane2, plane3, plane4, plane5, plane6, plane7, plane8, plane9]
 
     }
     
@@ -44,6 +62,7 @@ class PlaneTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         //Load the sample data.
         loadSamplePlanes()
@@ -87,14 +106,28 @@ class PlaneTableViewController: UITableViewController {
         return cell
     }
     
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        self.performSegue(withIdentifier: "showPlaneDetail", sender: self)
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        guard let identifier = segue.identifier else { return }
+        super.prepare(for: segue, sender: sender)
+
+        if segue.destination is PlaneViewController {
+            let teleport = segue.destination as? PlaneViewController
+            guard let selectedRowIndex = self.tableView.indexPathForSelectedRow else { return }
+            
+            teleport?.photo = planes[selectedRowIndex.item].photo
+            teleport?.model = planes[selectedRowIndex.item].model
+            teleport?.countryOfDesigning = planes[selectedRowIndex.item].country
+            teleport?.weightOfPlane = planes[selectedRowIndex.item].weight
+            teleport?.speedOfPlane = planes[selectedRowIndex.item].speed
+            teleport?.numberOfPassengers = planes[selectedRowIndex.item].numberOfPassengers
+            teleport?.distanceOfFlight = planes[selectedRowIndex.item].distance
+        }
     }
+}
+
+
+extension Double {
     
-    
+    var toString: String {
+        return NSNumber(value: self).stringValue
+    }
 }
--- Encyclopedia of planes/PlaneViewController.swift
+++ Encyclopedia of planes/PlaneViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import os.log
 
 class PlaneViewController: UIViewController {
 
@@ -26,29 +25,50 @@ class PlaneViewController: UIViewController {
     */
     var plane: Plane?
     
+    var photo: UIImage? = nil
+    var model: String = ""
+    var countryOfDesigning: String = ""
+    var weightOfPlane: Double = 0.0
+    var speedOfPlane: Double = 0.0
+    var numberOfPassengers: Int? = nil
+    var distanceOfFlight: Int? = nil
+    //let planeInfo = PlaneTableViewController()
     //MARK: Navigation
     
     //This method lets you configure a view controller before it's presented.
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        super.prepare(for: segue, sender: sender)
-        //It’s a good habit to always call super.prepare(for:sender:) whenever you override prepare(for:sender:)
-        
-        let photo = planeImage.image
-        let model = planeModel.text ?? ""
-        let countryOfDesigning = country.text ?? ""
-        let weightOfPlane = (weight.text! as NSString).doubleValue
-        let speedOfPlane = (speed.text! as NSString).doubleValue
-        let numberOfPassengers = Int(passengers.text!)
-        let distanceOfFlight = Int(distance.text!)
-        
-        plane = Plane(model: model, photo: photo, country: countryOfDesigning, weight: weightOfPlane, speed: speedOfPlane, passengers: numberOfPassengers!, distance: distanceOfFlight!)
-    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        super.prepare(for: segue, sender: PlaneTableViewController.self)
+//        //It’s a good habit to always call super.prepare(for:sender:) whenever you override prepare(for:sender:)
+//
+//        var photo: UIImage?
+//        var model: String
+//        var countryOfDesigning: String
+//        var weightOfPlane: Double
+//        var speedOfPlane: Double
+//        var numberOfPassengers: Int?
+//        var distanceOfFlight: Int?
+//
+//        photo = plane?.photo
+//        model = (plane?.model)!
+//        countryOfDesigning = (plane?.country)!
+//        weightOfPlane = plane?.weight ?? 0.0
+//        speedOfPlane = plane?.speed ?? 0.0
+//        numberOfPassengers = plane?.numberOfPassengers ?? 0
+//        distanceOfFlight = plane?.distance ?? 0
+//
+//        plane = Plane(model: model, photo: photo, country: countryOfDesigning, weight: weightOfPlane, speed: speedOfPlane, passengers: numberOfPassengers!, distance: distanceOfFlight!)
+//    }
     
     //MARK: Actions
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        planeImage.image = photo
+        planeModel.text = model
+        country.text = countryOfDesigning
+        weight.text = String(weightOfPlane)
+        speed.text = String(speedOfPlane)
+        passengers.text = String(numberOfPassengers ?? 0)
+        distance.text = String(distanceOfFlight ?? 0)
     }
-
-
 }
-
